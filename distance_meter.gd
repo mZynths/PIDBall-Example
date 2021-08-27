9a226a4450745b534137e4acdf060fcabc12cbff
Salvador Arana <mazl0nf3k@gmail.com>
2021-08-27 11:46:46 -0500

    Added simulated error

--- distance_meter.gd
+++ distance_meter.gd
@@ -1,15 +1,22 @@
 extends RayCast2D
 class_name DistanceMeter2D
 
-export var polling_rate := 0
-export var max_error := 0
+export var polling_rate := 0.5
+var elapsed_time = 0
+export var max_error := 0.0
 
 var result := 0.0
 
 func _physics_process(delta):
+	elapsed_time += delta
 	if is_colliding():
-		var origin = global_transform.origin
-		var collision_point = get_collision_point()
-		result = origin.distance_to(collision_point)
+		if elapsed_time >= polling_rate:
+			var origin = global_transform.origin
+			var collision_point = get_collision_point()
+			result = origin.distance_to(collision_point)
+			result += rand_range(-max_error, max_error)
+			elapsed_time = 0
 	else:
-		result = 0
+		result = 1000
+	
+#	print(result)
