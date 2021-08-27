bb244cea9e9c24112f84d289262ee8df04e36d6d
Salvador Arana <mazl0nf3k@gmail.com>
2021-08-27 11:47:22 -0500

    Removed useless code

--- Servo.gd
+++ Servo.gd
@@ -4,10 +4,9 @@ class_name Servo2D
 
 export var deg_setpoint := 0.0
 export var max_ang_velocity := 20.0
-
-var integral = 0
+export var max_ang = 180
+export var min_ang = 0
 
 func _physics_process(delta):
-	rotation_degrees = clamp(rotation_degrees, -90, 90)
 	angular_velocity = deg_setpoint - rotation_degrees
 	angular_velocity = clamp(angular_velocity, -max_ang_velocity, max_ang_velocity)
