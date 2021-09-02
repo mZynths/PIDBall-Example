0d07680ebef0fdca9266443b0e77bd1bac750a3f
Salvador Arana <mazl0nf3k@gmail.com>
2021-09-02 12:59:18 -0500

    Removed useless code

--- Servo.gd
+++ Servo.gd
@@ -7,9 +7,6 @@ export var max_ang_velocity := 20.0
 export var max_ang = 76
 export var min_ang = -104
 
-func _process(delta):
-	deg_setpoint = clamp(deg_setpoint, min_ang, max_ang)
-
 func _physics_process(delta):
 	angular_velocity = deg_setpoint - rotation_degrees
 	angular_velocity = clamp(angular_velocity, -max_ang_velocity, max_ang_velocity)
