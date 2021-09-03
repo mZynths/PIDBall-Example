025b462e33d93b7e013c44fae395febcc1255fef
Salvador Arana <mazl0nf3k@gmail.com>
2021-09-02 19:56:54 -0500

    Polling rate now on millis

--- distance_meter.gd
+++ distance_meter.gd
@@ -1,7 +1,7 @@
 extends RayCast2D
 class_name DistanceMeter2D
 
-export var polling_rate := 0.1
+export var polling_rate := 1 # in ms
 var elapsed_time = 0
 export var max_error := 0.0
 
@@ -13,7 +13,7 @@ var smoothing := 0
 func _physics_process(delta):
 	elapsed_time += delta
 	if is_colliding():
-		if elapsed_time >= polling_rate:
+		if elapsed_time >= polling_rate/1000.0:
 			var origin = global_transform.origin
 			var collision_point = get_collision_point()
 			result = origin.distance_to(collision_point)
