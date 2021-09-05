ee759b0348c07b34e570ef0521afe15bcd82c780
Salvador Arana <mazl0nf3k@gmail.com>
2021-09-05 13:59:32 -0500

    Added friction

--- Menu.gd
+++ Menu.gd
@@ -10,7 +10,7 @@ func _on_GravityVal_value_changed(value):
 	for ball in balls:
 		ball.gravity_scale = value
 
-func _on_MassVal_value_changed(value):
+func _on_FrictionVal_value_changed(value):
 	var balls = get_tree().get_nodes_in_group("Balls")
 	for ball in balls:
-		ball.mass = value
+		ball.friction = value
