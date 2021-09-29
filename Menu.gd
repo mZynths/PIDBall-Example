02985c74d37d2e38adc2278e7b5345f40de659d9
Salvador Arana <mazl0nf3k@gmail.com>
2021-09-29 12:22:03 -0500

    New tab "Values"

--- Menu.gd
+++ Menu.gd
@@ -3,7 +3,8 @@ extends VBoxContainer
 onready var tab_menu = $TabContainer
 
 func _ready():
-	tab_menu.set_tab_disabled(4, true)
+#	tab_menu.set_tab_disabled(4, true)
+	pass
 
 func _on_GravityVal_value_changed(value):
 	var balls = get_tree().get_nodes_in_group("Balls")
@@ -14,3 +15,7 @@ func _on_FrictionVal_value_changed(value):
 	var balls = get_tree().get_nodes_in_group("Balls")
 	for ball in balls:
 		ball.friction = value
+
+
+func _on_TabContainer_tab_changed(tab):
+	get_node("Error").visible = !(tab == 4)
